11f49f581fea326ffb091c256acf9fe9c43455e1
Ubuntu <ubuntu@ip-172-31-92-79.ec2.internal>
2020-11-18 17:07:24 +0000

    add nice navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@example.com', password: 'Disc0ver', password_confirmation: 'Disc0ver')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
